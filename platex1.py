cd0ee3b6b5657e265e7de5df1c139642ebcba47a
MauxMaribel <mauxmaribel@gmail.com>
2016-09-14 00:50:53 -0400

    update to platex

--- platex1.py
+++ platex1.py
@@ -1,130 +1,114 @@
 import pygame
 
-black = (0, 0, 0)
-white = (255, 255, 255)
-green = (0, 255, 0)
-
-screen_width = 800
-screen_length = 600
-
-class Player(pygame.sprite.Sprite):
-	
-	def __init__(self, x, y):
-		super().__init__()
-		
-		self.image = pygame.Surface([15, 15])
-		self.image.fill(white)
-		
-		self.rect = self.image.get_rect()
-		self.rect.y = y
-		self.rect.x = x
-		
-		self.change_x = 0
-		self.change_y = 0
-		self.walls = None
-		
-	def changespeed(self, x, y):
-		self.change_x += x
-		self.change_y += y
-		
-	def update(self):
-		self.rect.x += self.change_x
-		
-		block_hit_list = pygame.sprite.spritecollide(self, self.walls, False)
-		for block in block_hit_list:
-			if self.change_x > 0:
-				self.rect.right = block.rect.left
-			else:
-				self.rect.left = block.rect.right
-				
-		self.rect.y += self.change_y
-		
-		for block in block_hit_list:
-			if self.change_y > 0:
-				self.rect.bottom = block.rect.top
-			else:
-				self.rect.top = block.rect.bottom
-				
-class Wall(pygame.sprite.Sprite):
-
-	def __init__(self, x, y, width, height):
-		super().__init__()
-		
-		self.image = pygame.Surface([width, height])
-		self.image.fill(green)
-		
-		self.rect = self.image.get_rect()
-		self.rect.y = y
-		self.rect.x = x
-		
 pygame.init()
 
-screen = pygame.display.set_mode([screen_width, screen_length])
+black = (0, 0, 0)
+white = (255, 255, 255)
+blue = (0, 0, 255)
+green = (0, 255, 0)
+red = (255, 0, 0)
 
-pygame.display.set_caption("Test")
+size = [700, 500]
+screen = pygame.display.set_mode(size)
 
-all_sprite_list = pygame.sprite.Group()
+pygame.display.set_caption("Test Rectangle")
 
-wall_list = pygame.sprite.Group()
-
-wall = Wall(0, 0, 10, 600)
-wall_list.add(wall)
-all_sprite_list.add(wall)
-
-wall = Wall(10, 0, 790, 10)
-wall_list.add(wall)
-all_sprite_list.add(wall)
-
-wall = Wall(10, 200, 100, 10)
-wall_list.add(wall)
-all_sprite_list(wall)
-
-player = Player(50, 50)
-player.walls = wall_list
-
-all_spite_list.add(player)
+done = False 
 clock = pygame.time.Clock()
 
-done = False
+pygame.mouse.set_visible(0)
+
+x_speed = 0
+y_speed = 0
+
+x_coord = 10
+y_coord = 10
+
+
+
+
+platforms = []
+
+def RectOverLap(rect_a, rect_b):
+
+	x_apart = rect_a[0] > rect_b[0] + rect_b[2] or rect_b[0] > rect_a[0] + rect_a[2]
+	y_apart = rect_a[1] > rect_b[1] + rect_b[3] or rect_b[1] > rect_a[1] + rect_a[3]
+	
+	return x_apart == False and y_apart == False
+
+
+
+def draw_rect(screen, x, y):
+
+	pygame.draw.rect(screen, white, [x, y, 50, 20])
+	
+def draw_platform(screen, x, y, w, h):
+
+	pygame.draw.rect(screen, green, [x, y, w, h])
+
 
 while not done:
-	
+
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			done = True
 			
 		elif event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_a:
-				player.changespeed(-3, 0)
+				x_speed = -3
 			elif event.key == pygame.K_d:
-				player.changespeed(3,0)
-			elif event.key == pygame.K_w:
-				player.changespeed(0, -3)
-			elif event.key == pygame.K_s:
-				player.changespeed(0, 3)
-				
-		elif event.type == pygame.KEYUP:
-			if event.key == pygame.K_a:
-				player.changespeed(3,0)
-			elif event.key == pygame.K_d:
-				player.changespeed(-3, 0)
-			elif event.key == pygame.K_w:
-				player.changespeed(0, 3)
-			elif event.key == pygame.K_s:
-				player.changespeed(0, -3)
+				x_speed = 3
+			elif event.key == pygame.K_w and y_speed == 0:
+				y_speed = -10
 			
-	all_sprite_list.update()
+		elif event.type == pygame.KEYUP:
+			if event.key == pygame.K_a or event.key == pygame.K_d:
+				x_speed = 0
+
+				
+
+
+	y_speed = y_speed + 0.3
+	x_coord = x_coord + x_speed
+	y_coord = y_coord + y_speed
 			
 	screen.fill(black)
-
-	all_sprite_list.draw(screen)
 	
+	draw_rect(screen, x_coord, y_coord)
+	
+	draw_platform(screen, 400, 350, 100, 25)
+	draw_platform(screen, 0, 475, 700, 25)
+	
+	player = [x_coord, y_coord, 50, 20]
+	plat_a = [400, 350, 100, 25]
+	plat_b = [0, 475, 700, 25]
+	
+	if RectOverLap(plat_a, player) == True:
+		
+		y_coord = y_coord - y_speed
+		y_speed = 0
+				
+			
+	elif RectOverLap(plat_b, player) == True:
+		y_speed = 0
+		y_coord = 455
+		
+			
+			
 	pygame.display.flip()
+	
+	
+	if x_coord < 0:
+		x_coord = 0
+	elif x_coord + 50 > 700:
+		x_coord = 700 - 50
+	elif y_coord < 0:
+		y_coord = 0
+	elif y_coord + 20 > 500:
+		y_coord = 500 - 20
+
 	
 	clock.tick(60)
 	
+	
 pygame.quit()
-			
-			
-			
-			
